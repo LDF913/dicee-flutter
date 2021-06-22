e4d6dafd4599f6381f1ad3e0811e74baf8cf9f83
LDF913 <ldf913@gmail.com>
2021-06-22 16:47:50 -0400

    change color added functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.white24,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Center(child: Text('Dicee')),
+          backgroundColor: Colors.deepPurpleAccent,
         ),
         body: DicePage(),
       ),
@@ -15,9 +16,60 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  void NumberRoll() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+      leftDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+// easy way of adding image widget
+            child: FlatButton(
+              onPressed: () {
+                NumberRoll();
+
+                print(
+                  'left button pressed',
+                );
+              },
+              child: Image.asset(
+                'images/dice$leftDiceNumber.png',
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                NumberRoll();
+
+                print(
+                  'right button pressed',
+                );
+              },
+              child: Image(
+                image: AssetImage(
+                  'images/dice$rightDiceNumber.png',
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
